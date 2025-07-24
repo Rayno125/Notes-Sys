fdca3cb32f80ea5aac24c468921a58084f0e22c0
Rayno125 <125220828+Rayno125@users.noreply.github.com>
2025-07-24 22:18:50 +0400

    database #1
    added Notes and Users database

--- app.py
+++ app.py
@@ -1,10 +1,64 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from models import Note, User
+from db_config import db
+
+
+
+
+
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+
+app.config['SQALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+# db = SQLAlchemy(app)
+
+
 
 @app.route('/')
 
 def index():
     return render_template('index.html')
 
-app.run(debug=True)
+
+
+@app.route('/add/<username>')
+def add_note(username):
+    user = User.query.filter_by(username=username).first()
+
+    
+    
+    new_note = Note(title="Пример", content="Это содержимое", user_id=user.id)
+    db.session.add(new_note)
+    db.session.commit()
+    return f"Заметка добавлена на аккаунт {username}"
+        
+
+@app.route('/notes/<username>')
+def get_notes(username):
+    user = User.query.filter_by(username=username).first()
+
+    notes = Note.query.filter_by(user_id=user.id).all()
+
+    
+    
+    return {note.id: note.title + '/'+ note.content for note in notes}
+  
+@app.route('/create_user/<username>')
+def create_user(username):
+    user = User(username=username)
+    db.session.add(user)
+    db.session.commit()
+    return f'Пользователь {username} успешно создан!'
+
+
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
+
---
+++ models.py
@@ -0,0 +1,22 @@
+from db_config import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True,nullable=False)
+
+    notes = db.relationship('Note', backref = 'user', lazy=True)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'),nullable=False)
+
+    def __repr__(self):
+        return f'<Note{self.title}>'
+    
